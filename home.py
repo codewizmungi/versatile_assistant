c5a3a017c95a3e87da691653ccc5cc80634beecc
Mungwa Agu <mungwaagu@gmail>
2024-03-21 18:00:02 +0100

    Updated home.py

--- home.py
+++ home.py
@@ -6,7 +6,7 @@ st.set_page_config(
     menu_items={
         'About': "https://isitnet.com"
     })
-st.title("Hello, How can I assist you today?")
+st.header("Hello, how can I assist you today?")
 st.text("")
 st.markdown("Here are some suggestions to get you started")
 
