df4c8ca27ab516304a97ab700c0ab552facd002e
Mungwa Agu <mungwaagu@gmail>
2024-06-18 11:46:29 +0100

    Changed name of home.py

--- pages/chat.py
+++ pages/chat.py
@@ -45,7 +45,7 @@ with open('.streamlit/css/chat.css') as f:
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
 ## function to load Gemini 1.0 Pro model and get response
-model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=generation_config,safety_settings=safety_settings)
+model = genai.GenerativeModel(model_name="gemini-1.5-pro", generation_config=generation_config,safety_settings=safety_settings)
 
 chat = model.start_chat(history=[
   {
